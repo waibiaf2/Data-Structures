8c09905e3b9c41245cc0b360d34ad515d521a1e4
Waibi Andrew Franklin <waibiaf2@gmail.com>
2024-12-22 14:03:07 +0300

    Complete array practice

--- src/practice/Array.java
+++ src/practice/Array.java
@@ -1,5 +1,56 @@
 package practice;
 
 public class Array {
-    
+    int[] items;
+    int count = 0;
+
+    Array(int length) {
+        items = new int[length];
+    }
+
+    public void insert(int item) {
+        if(count == items.length) {
+            int newItems[] = new int[items.length * 2];
+
+            System.arraycopy(items, 0, newItems, 0, count);
+
+            newItems[count++] = item;
+            items = newItems;
+            return;
+        }
+
+        items[count++] = item;
+    }
+
+    //removeAt
+    public void removeAt(int index) {
+        // validate the index
+        if(index < 0 || index > count)
+            throw new IllegalArgumentException();
+        
+        for (int i = index; i < count; i++) {
+            items[i] = items[i + 1];            
+        }
+
+        count--;
+    }
+
+    //indexOf
+    public int indexOf(int item) {
+
+        for (int i = 0; i < count; i++) {
+            if(items[i] == item ) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    //print
+    public void print() {
+        for (int i = 0; i < items.length; i++) {
+            System.out.println(items[i]);
+        }
+    }
 }
